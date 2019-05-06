52cb2845b36c0418d05b5386ca322ac0eae8af8f
wnqui <49946406+wnqui@users.noreply.github.com>
2019-05-06 22:25:33 +0800

    set framework

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,53 @@
 
 public class Node {
 
+	int x;
+	int y;
+
+	double f;
+	double g;
+	double h;
+
+	Node(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public void setH(Node goal) {
+
+		this.h = Math.abs(goal.x - this.x) + Math.abs(goal.y - this.y);
+
+	}
+	
+
+	public Node[] neighbor(Mazu mazu) {
+		Node[] neighbor = new Node[8];
+
+		if (mazu.map[x][y - 1] == 0)
+			neighbor[0] = new Node(x, y - 1);
+		
+		if (mazu.map[x + 1][y - 1] == 0)
+			neighbor[1] = new Node(x + 1, y - 1);
+		
+		if (mazu.map[x + 1][y] == 0)
+			neighbor[2] = new Node(x + 1, y);
+		
+		if (mazu.map[x + 1][y + 1] == 0)
+			neighbor[3] = new Node(x + 1, y + 1);
+		
+		if (mazu.map[x][y + 1] == 0)
+			neighbor[4] = new Node(x, y + 1);
+		
+		if (mazu.map[x - 1][y + 1] == 0)
+			neighbor[5] = new Node(x - 1, y + 1);
+		
+		if (mazu.map[x - 1][y] == 0)
+			neighbor[6] = new Node(x - 1, y);
+		
+		if (mazu.map[x - 1][y - 1] == 0)
+			neighbor[7] = new Node(x - 1, y - 1);
+
+		return neighbor;
+	}
+
 }
--- src/Path.java
+++ src/Path.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
 
 public class Path {
+	
+	ArrayList<Node> open ;
+	ArrayList<Node> colse ;
+	
+	public Node[] pathfinding(Node start , Node goal , Mazu map) {
+		
+		open = new ArrayList<Node>();
+		colse = new ArrayList<Node>();
+		
+		open.add(start);
+		
+		while(open.size() != 0) {
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		
+		
+		return null;
+		
+	}
+	
+
+	
 
 }
